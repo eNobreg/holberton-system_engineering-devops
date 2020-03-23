ed22cecbdf81d120f3ba6041e35fdd375a18472e
eNobreg <ezra.nobrega@outlook.com>
2020-03-23 16:09:28 -0400

    [New] Initial push

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Module to create a file
 file {'/tmp/holberton':
 	ensure  => file,
-        path    => '/etc/holberton',
+        path    => '/tmp/holberton',
         mode    => '0744',
         group   => 'www-data',
         owner   => 'www-data',
