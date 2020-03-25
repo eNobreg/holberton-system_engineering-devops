9212638105446e4113cafc58456b260782a00301
eNobreg <ezra.nobrega@outlook.com>
2020-03-24 22:01:39 -0400

    [New] Initial push

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# Automatic Configuration
+file_line {'no_auth':
+  ensure => 'present',
+  path   => '~/.ssh/config',
+  line   => 'PasswordAuthentication no',
+        }
+file_line {'identity_file':
+  ensure => 'present',
+  path   => '~/.ssh/config'
+  line   => 'IdentityFile ~/.ssh/holberton'
+        }
