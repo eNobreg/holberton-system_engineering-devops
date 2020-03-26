c06549495d22aeb61688210a6e4c7e5ec3a41fa3
eNobreg <ezra.nobrega@outlook.com>
2020-03-26 16:06:27 -0400

    [Removed] Parentheses

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 # Install
 exec{ 'Install':
-  command  => 'sudo apt-get update && sudo apt-get -y install nginx && (echo "Holberton School for the win!" | sudo tee /var/www/html/index.nginx-debian.html) && sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default-backup && (redirect="\n\tserver_name ezracruz.tech;\n\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;") && sudo sed -i "37i\ $redirect" /etc/nginx/sites-available/default && sudo service nginx start',
+  command  => 'sudo apt-get update && sudo apt-get -y install nginx && echo "Holberton School for the win!" | sudo tee /var/www/html/index.nginx-debian.html && sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default-backup && redirect="\n\tserver_name ezracruz.tech;\n\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" && sudo sed -i "37i\ $redirect" /etc/nginx/sites-available/default && sudo service nginx start',
   provider => shell,
 }
