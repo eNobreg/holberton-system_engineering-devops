e2a609e1f52d74ef4aade2e96fd9d2906fc17bbd
eNobreg <ezra.nobrega@outlook.com>
2020-04-03 19:36:17 -0400

    [New] Initial push

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,5 +1,5 @@
 # This
 exec { 'Install':
-    command  => 'sudo apt-get update && sudo apt-get install -y nginx && sudo sed -i "35i\ \tadd_header X-Served-By $HOSTNAME;\n" /etc/nginx/nginx.conf && sudo service nginx restart',
+    command  => 'sudo apt-get update && sudo apt-get install -y nginx && sudo sed -i "35i\ \tadd_header X-Served-By $HOSTNAME;\n" /etc/nginx/sites-available/default && sudo service nginx restart',
     provider => shell,
 }
