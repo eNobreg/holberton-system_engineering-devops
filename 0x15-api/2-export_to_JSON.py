a1224dd30e688012bc373423115a660916efdf69
eNobreg <ezra.nobrega@outlook.com>
2020-05-04 16:30:13 -0400

    [Fix] Pep8 check

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -2,19 +2,22 @@
 """
 Module for retreiving info from API
 """
-import json
-import requests
-from sys import argv
+if __name__ == "__main__":
+    import json
+    import requests
+    from sys import argv
 
-dict_list = []
-json_dict = {}
-url = "https://jsonplaceholder.typicode.com/users/{}/".format(argv[1])
-user = requests.get(url).json()["username"]
-tasks = requests.get(url + "todos/").json()
-for task in tasks:
-    dict_list.append({"task": task["title"], "completed": task["completed"], "username": user})
+    dict_list = []
+    json_dict = {}
+    url = "https://jsonplaceholder.typicode.com/users/{}/".format(argv[1])
+    user = requests.get(url).json()["username"]
+    tasks = requests.get(url + "todos/").json()
+    for task in tasks:
+        dict_list.append({"task": task["title"], "completed":
+                         task["completed"],
+                         "username": user})
 
-json_dict.update({argv[1]: dict_list})
-obj = json.dumps(json_dict)
-with open("{}.json".format(argv[1]), "w") as file:
-    file.write(obj)
+    json_dict.update({argv[1]: dict_list})
+    obj = json.dumps(json_dict)
+    with open("{}.json".format(argv[1]), "w") as file:
+        file.write(obj)
