9349c01da465d017c421f8773c80206b386e0104
eNobreg <ezra.nobrega@outlook.com>
2020-05-04 15:41:21 -0400

    [New] Initial push

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,18 +5,19 @@ Module for retreiving info from API
 import requests
 from sys import argv
 
-completed = 0
-total = 0
-completed_titles = []
-url = "https://jsonplaceholder.typicode.com/users/{}/".format(argv[1])
-user = requests.get(url).json()["name"]
-tasks = requests.get(url + "todos/").json()
-for task in tasks:
-    if task["completed"] is True:
-        completed += 1
-        completed_titles.append(task["title"])
-    total += 1
+if __name__ == "__main__":
+    completed = 0
+    total = 0
+    completed_titles = []
+    url = "https://jsonplaceholder.typicode.com/users/{}/".format(argv[1])
+    user = requests.get(url).json()["name"]
+    tasks = requests.get(url + "todos/").json()
+    for task in tasks:
+        if task["completed"] is True:
+            completed += 1
+            completed_titles.append(task["title"])
+        total += 1
 
-print("Employee {} is done with tasks({}/{}):".format(user, completed, total))
-for title in completed_titles:
-    print("\t{}".format(title))
+    print("Employee {} is done with tasks({}/{}):".format(user, completed, total))
+    for title in completed_titles:
+        print("\t{}".format(title))
