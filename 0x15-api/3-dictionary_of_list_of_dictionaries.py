5d2d3d85f8d83494ff9345fba46ff0b70d8de24e
eNobreg <ezra.nobrega@outlook.com>
2020-05-04 14:52:58 -0400

    [Fix] Pep8 check

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -13,12 +13,13 @@ for user in users:
     user_id_list.append(user["id"])
 
 for uid in user_id_list:
-    dict_list = [] 
+    dict_list = []
     url = "https://jsonplaceholder.typicode.com/users/{}/".format(uid)
     user = requests.get(url).json()["username"]
     tasks = requests.get(url + "todos/").json()
     for task in tasks:
-        dict_list.append({"username": user, "task": task["title"], "completed": task["completed"]})
+        dict_list.append({"username": user, "task": task["title"], "completed":
+                          task["completed"]})
     json_dict.update({uid: dict_list})
 
 obj = json.dumps(json_dict)
