6c9f5dd6cec1737f667594d76649bb74152105cf
eNobreg <ezra.nobrega@outlook.com>
2020-05-04 16:32:45 -0400

    [Fix] Pep8 check

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,28 +2,30 @@
 """
 Module for retreiving info from API
 """
-import json
-import requests
-from sys import argv
 
-json_dict = {}
-user_id_list = []
-users = requests.get("https://jsonplaceholder.typicode.com/users/").json()
-for user in users:
-    user_id_list.append(user["id"])
+if __name__ == "__main__":
+    import json
+    import requests
+    from sys import argv
 
-for uid in user_id_list:
-    dict_list = []
-    url = "https://jsonplaceholder.typicode.com/users/{}/".format(uid)
-    user = requests.get(url).json()["username"]
-    tasks = requests.get(url + "todos/").json()
-    for task in tasks:
-        dict_list.append({"username": user, "task": task["title"], "completed":
-                          task["completed"]})
-    json_dict.update({uid: dict_list})
+    json_dict = {}
+    user_id_list = []
+    users = requests.get("https://jsonplaceholder.typicode.com/users/").json()
+    for user in users:
+        user_id_list.append(user["id"])
 
-obj = json.dumps(json_dict)
+    for uid in user_id_list:
+        dict_list = []
+        url = "https://jsonplaceholder.typicode.com/users/{}/".format(uid)
+        user = requests.get(url).json()["username"]
+        tasks = requests.get(url + "todos/").json()
+        for task in tasks:
+            dict_list.append({"username": user, "task": task["title"],
+                              "completed":
+                              task["completed"]})
+        json_dict.update({uid: dict_list})
 
+    obj = json.dumps(json_dict)
 
-with open("todo_all_employees.json", "w") as file:
-    file.write(obj)
+    with open("todo_all_employees.json", "w") as file:
+        file.write(obj)
