5ac820d0a1025d92aaca35fa4863a29db2340b48
eNobreg <ezra.nobrega@outlook.com>
2020-05-07 01:06:17 -0400

    [New] Initial push

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module to request subreddit sub count """
+
+def number_of_subscribers(subreddit):
+	""" Return the number of subscribers of a subreddit """
+	import requests
+	import json
+	from sys import argv
+
+	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+	user_agent = "person_at_school"
+	header = {'User-Agent': user_agent}
+	resp = requests.get(url, headers=header, allow_redirects=False)
+	if (resp.status_code == 302):
+		return 0
+	else:
+		return(resp.json()['data']['subscribers'])
