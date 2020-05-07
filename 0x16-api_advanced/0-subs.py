a0db933fe5c04c3dbfe929491e203dfcca052078
eNobreg <ezra.nobrega@outlook.com>
2020-05-07 12:35:08 -0400

    [Fix] Pep8 check

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """ Module to request subreddit sub count """
 
-def number_of_subscribers(subreddit):
-	""" Return the number of subscribers of a subreddit """
-	import requests
-	import json
-	from sys import argv
 
-	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-	user_agent = "person_at_school"
-	header = {'User-Agent': user_agent}
-	resp = requests.get(url, headers=header, allow_redirects=False)
-	if (resp.status_code == 302):
-		return 0
-	else:
-		return(resp.json()['data']['subscribers'])
+def number_of_subscribers(subreddit):
+    """ Return the number of subscribers of a subreddit """
+    import requests
+    import json
+    from sys import argv
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    user_agent = "person_at_school"
+    header = {'User-Agent': user_agent}
+    resp = requests.get(url, headers=header, allow_redirects=False)
+    if (resp.status_code == 302):
+        return 0
+    else:
+        return(resp.json()['data']['subscribers'])
